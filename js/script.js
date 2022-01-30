783dffc18f280b0b3902de3a7b81071ebe943486
Николай Малыгин <nikolos.f1@gmail.com>
2022-01-30 15:20:37 +0500

    replace index.html style.css carousel.css script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,11 @@
-let countDownDate = new Date;
-
-// Update the count down every 1 second
+let countDownDate = new Date().getTime() + 1800000;
 let x = setInterval(function() {
-
-    // Get todays date and time
     let now = new Date().getTime();
-
-    // Find the distance between now an the count down date
-    let distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    // let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    // let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let distance = countDownDate - now ;
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById("countdown").innerHTML =  minutes + "m " + seconds + "s ";
-
-    // If the count down is finished, write some text
+    document.getElementById("countdown").innerHTML ="Countdown: " + hours + "h " + minutes + "m " + seconds + "s ";
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "EXPIRED";
